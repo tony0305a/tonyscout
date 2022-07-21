27ae673e78bb251b9cd90c0d05c005d69f853040
Tony <luiz.santos@iqb.ufal.br>
2022-07-20 23:13:42 -0300

    add router, privacy policy and use terms

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import * as S from "./AppStyle";
-import Navigation from "./components/Navigation/Navigation";
 import { useEffect, useState } from "react";
 import { Home } from "./components/Home/Home";
-import logo from './imgs/logo.png'
 import useScout from "./hooks/riot-hook";
 function App() {
   const [renderProfile, setRenderProfile] = useState(false);
-  const {scoutState, cleanScoutState, cleanMatchData, cleanMasteriesState, cleanRankedState,} = useScout()
+  const {
+    scoutState,
+  } = useScout();
 
-  const rHomePage = () =>{
-    cleanScoutState()
-    cleanMasteriesState()
-  }
-  useEffect(()=>{
-    if(scoutState.hasUser){
-      setRenderProfile(true)
+
+  useEffect(() => {
+    if (scoutState.hasUser) {
+      setRenderProfile(true);
     }
-  },[scoutState])
+  }, [scoutState]);
 
   return (
     <>
-      <S.Nav>
-        <img src={logo}  onClick={rHomePage} width="64" height="64" />
-      </S.Nav>
-      <Header />
+    <Header/>
+      {scoutState.hasUser ? (
+        <>
+          <Profile />
+        </>
+      ) : (
+        <>
+          <Home />
+        </>
+      )}
 
-      {scoutState.hasUser?(<><Profile /></>):(<><Home /></>)}
-      
-      <S.Wrapper></S.Wrapper>
+      <S.Wrapper>
+      </S.Wrapper>
+
     </>
   );
 }
---
+++ src/pages/get-oauth-callback.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../components/Footer/Footer";
+
+const GetOauthCallback = () =>{
+    return(
+        <>
+        <p>Get oauth callback</p>
+        <Footer/>
+        </>
+    )
+}
+export default GetOauthCallback
