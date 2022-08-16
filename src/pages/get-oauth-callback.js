d7c6504ce51256585d65ea5dd0a7432906a40d5d
Tony <luiz.santos@iqb.ufal.br>
2022-08-16 00:04:54 -0300

    test

--- src/pages/get-oauth-callback.js
+++ src/pages/get-oauth-callback.js
@@ -7,9 +7,21 @@ import { useLocation } from "react-router-dom";
 
 const GetOauthCallback = () => {
     const search = useLocation().search;
-    const code =new URLSearchParams(search).get("code");
+    const codeQuery = new URLSearchParams(search).get("code");
 
-    
+
+
+const options = {
+  method: 'GET',
+  url: 'https://the-one-who-searches.herokuapp.com/oauth-callback',
+  data: {code: codeQuery}
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
 
 
   return (
