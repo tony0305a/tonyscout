49cf64181138b31beb62819d57f67c614b72dfc1
Tony <luiz.santos@iqb.ufal.br>
2022-08-16 00:06:40 -0300

    test

--- src/pages/get-oauth-callback.js
+++ src/pages/get-oauth-callback.js
@@ -1,32 +1,29 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import axios from "axios";
-import request from 'request'
 import { useLocation } from "react-router-dom";
 
-
 const GetOauthCallback = () => {
-    const search = useLocation().search;
-    const codeQuery = new URLSearchParams(search).get("code");
+  const search = useLocation().search;
+  const codeQuery = new URLSearchParams(search).get("code");
 
+  const options = {
+    method: "GET",
+    url: "https://the-one-who-searches.herokuapp.com/oauth-callback",
+    data: { code: codeQuery },
+  };
 
-
-const options = {
-  method: 'GET',
-  url: 'https://the-one-who-searches.herokuapp.com/oauth-callback',
-  data: {code: codeQuery}
-};
-
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
-
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 
   return (
     <>
-    <span>{id}</span>
       <button>test</button>
       <Footer />
     </>
